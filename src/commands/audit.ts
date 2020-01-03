e926135ea04a2400d8f085f3f3665ce794601993
Roman Vesely <crazko@gmail.com>
2020-01-03 12:45:12 +0100

    Add report to audit command

--- src/commands/audit.ts
+++ src/commands/audit.ts
@@ -1,28 +1,63 @@
-import { Command } from '@oclif/command';
+import * as fs from 'fs';
+import { Command, flags } from '@oclif/command';
 import * as execa from 'execa';
 
 export default class Audit extends Command {
   static description = 'Report vulnerabilities';
 
+  static flags = {
+    report: flags.boolean({ char: 'r', helpLabel: '--report, -r', description: 'Generates HTML report.' }),
+  };
+
   async run() {
-    const command = `npm audit --audit-level=critical`;
+    const { flags } = this.parse(Audit);
+    const report = flags.report;
+
+    const command = ['npm audit', ...(report ? ['--json'] : []), '--audit-level=low'].join(' ');
 
     this.log(command);
 
     try {
-      const subprocess = await execa.command(
-        command,
-        // { env: { FORCE_COLOR: 'true' } }
-        { stdio: 'inherit' }
-      );
-      this.log(subprocess.stdout);
-    } catch ({ stdout, stderr, exitCode }) {
-      if (stderr) {
-        this.error(stderr, { code: exitCode });
-      } else {
-        this.log(stdout);
-        this.exit(exitCode);
+      await execa.command(command);
+      this.log('No vulnerabilities found');
+    } catch ({ exitCode, stdout }) {
+      if (report) {
+        const filePath = createReport(stdout);
+
+        this.error(`Vulnerabilities found. Report generated at ${filePath}`, {
+          exit: exitCode,
+        });
       }
+
+      this.log(stdout);
+      this.exit(exitCode);
     }
   }
 }
+
+/**
+ * @return report file destination
+ */
+const createReport = (stdout: string, path = './test-results') => {
+  const title = 'Vulnerabilities Report';
+  const output = `<html lang="en">
+  <head>
+    <title>${title}</title>
+    <style>html { font-family: monospace }</style>
+  </head>
+  <body>
+    <h1>${title}</h1>
+    <pre>${stdout}</pre>
+  </body>
+</html>`;
+
+  if (!fs.existsSync(path)) {
+    fs.mkdirSync(path);
+  }
+
+  const filePath = `${path}/reports-audit.html`;
+
+  fs.writeFileSync(filePath, output);
+
+  return filePath;
+};
