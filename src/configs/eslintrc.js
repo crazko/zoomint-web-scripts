0c9e99dcebe31d7da200508cb5c01568dccd70b7
Roman Vesely <crazko@gmail.com>
2020-02-29 11:09:36 +0100

    Update React rules in TypeScript

--- src/configs/eslintrc.js
+++ src/configs/eslintrc.js
@@ -22,10 +22,6 @@ module.exports = {
     // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/no-unresolved.md
     'import/no-unresolved': 'off',
 
-    // https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/explicit-function-return-type.md
-    '@typescript-eslint/explicit-function-return-type': 'off',
-
-    // Add TypeScript extensions
     // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/extensions.md
     'import/extensions': [
       'error',
@@ -37,5 +33,20 @@ module.exports = {
         tsx: 'never',
       },
     ],
+
+    // https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/explicit-function-return-type.md
+    '@typescript-eslint/explicit-function-return-type': 'off',
+
+    // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-filename-extension.md
+    'react/jsx-filename-extension': ['error', { extensions: ['.jsx', '.tsx'] }],
   },
+  overrides: [
+    {
+      files: ['**/*.tsx'],
+      rules: {
+        // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/prop-types.md
+        'react/prop-types': 'off',
+      },
+    },
+  ],
 };
