c83e0cf4487e4d02237a38c32b4a523f66a18dbb
Roman Vesely <crazko@gmail.com>
2020-03-05 19:38:01 +0100

    feat(lint): Update default rules

--- src/configs/eslintrc.js
+++ src/configs/eslintrc.js
@@ -13,6 +13,9 @@ module.exports = {
     'prettier/react',
   ],
   rules: {
+    // https://eslint.org/docs/rules/no-nested-ternary
+    'no-nested-ternary': 'warn',
+
     // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/prefer-default-export.md
     'import/prefer-default-export': 'off',
 
@@ -34,12 +37,44 @@ module.exports = {
       },
     ],
 
+    // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/no-extraneous-dependencies.md
+    // https://github.com/airbnb/javascript/blob/master/packages/eslint-config-airbnb-base/rules/imports.js#L71
+    'import/no-extraneous-dependencies': [
+      'error',
+      {
+        devDependencies: [
+          '**/__mocks__/**', // jest pattern
+          '**/*{.,_}{test,spec}.{js,jsx,ts,tsx}', // tests where the extension or filename suffix denotes that it is a test
+          '**/jest.config.js',
+          '**/jest.setup.js',
+          '**/webpack.config.js',
+          '**/webpack.config.*.js',
+          '**/rollup.config.js',
+          '**/rollup.config.*.js',
+        ],
+        optionalDependencies: false,
+      },
+    ],
+
     // https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/explicit-function-return-type.md
     '@typescript-eslint/explicit-function-return-type': 'off',
 
+    // https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/no-unused-vars.md
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+
     // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-filename-extension.md
     'react/jsx-filename-extension': ['error', { extensions: ['.jsx', '.tsx'] }],
+
+    // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/state-in-constructor.md
+    'react/state-in-constructor': 'off',
+
+    // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-props-no-spreading.md
+    'react/jsx-props-no-spreading': 'off',
+
+    // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/static-property-placement.md
+    'react/static-property-placement': 'warn',
   },
+
   // TODO: this has to be overriden in the top level config
   // Issue: https://github.com/eslint/eslint/issues/11558, https://github.com/eslint/eslint/pull/12887
   overrides: [
